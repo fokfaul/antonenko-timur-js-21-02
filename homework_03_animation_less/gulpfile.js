d6a9b7bcc5d2ff081296d91854b05e44ee1bf9a8
fokfaul <91566696+fokfaul@users.noreply.github.com>
2021-10-07 18:17:43 +0200

    Revert "Develop"

--- homework_03_animation_less/gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const less = require('gulp-less');
-
-gulp.task('less', function(done){
-    gulp.src('./less/**/*.less')
-        .pipe(less())
-        .pipe(gulp.dest('./css'))
-    done();
-})
