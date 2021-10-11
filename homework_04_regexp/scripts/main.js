17948284691f161f5fa18ac3cad4f74a034a71ee
fokfaul <fokfaul@mail.ru>
2021-10-11 21:49:02 +0300

    Добавление п. 3 и 4

--- homework_04_regexp/scripts/main.js
+++ homework_04_regexp/scripts/main.js
@@ -1,5 +1,5 @@
 /*const firstStr = prompt("1. Введите две строки через запяту.").split(',',2);
-alert("1. "+(firstStr[0].toLowerCase()===firstStr[1].toLowerCase()));*/
+alert("1. "+(firstStr[0].toLowerCase()===firstStr[1].toLowerCase()));
 
 function truncate(str, maxlength) {
   return (str.length > maxlength) ?
@@ -8,3 +8,9 @@ function truncate(str, maxlength) {
 const secondStr = prompt("2. Введите строку.");
 const secondNum = prompt("2. Введите число.");
 alert("2. "+truncate(secondStr, secondNum));
+
+const thirdStr = prompt("3. Введите дату в формате dd/mm/year hh-mm");
+alert("3. "+thirdStr.replace(new RegExp("/",'g'),'.').replace(/-/,':'))*/
+
+const fourthStr = prompt("4. Введите ФИО.");
+alert("4. "+(/^[А-Яа-я]+ [А-Яа-я]+( ([А-Яа-я]+вич)| ([А-Яа-я]+вна))*$/.test(fourthStr)));
