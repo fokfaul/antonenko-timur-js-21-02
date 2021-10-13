fddb05d15c0140d615a28701a220f4287d3307dc
fokfaul <fokfaul@mail.ru>
2021-10-13 17:22:35 +0300

    Добавление п. 3 и 4

--- homework_05_massive/scripts/main.js
+++ homework_05_massive/scripts/main.js
@@ -1,3 +1,4 @@
+/*
 // 1. На вход поступает массив, вывести массив, удалив неуникальные значения.
  const first_inArray = [1, 2, 5, 6, 11, 5, '1', '1'];
  console.log(first_inArray.filter((value, index, arr) => {return arr.indexOf(value) === index}));
@@ -10,4 +11,41 @@ for(let i=0,j; i*2<second_inArray.length; i++)
     second_inArray[i] = second_inArray[second_inArray.length-1-i];
     second_inArray[second_inArray.length-1-i] = j;
 }
-console.log(second_inArray);
+console.log(second_inArray);*/
+
+/* 3. На вход поступает массив, содержащий массивы, в которых хранится два элемента. Преобразовать массив в объект,
+где ключами являются нулевой индекс вложенных массивов, а значениями являются элементы с индексом один.*/
+
+const third_inArray = [["one", 2], ["two", 4], ["three", 6], ["four", 8]];
+const third_outObject = {};
+third_inArray.forEach((arr) => third_outObject[arr[0]] = arr[1]);
+console.log(third_outObject);
+
+// 4. На вход поступает объект, вывести сумму числовых свойств объекта.
+
+const fourth_inObject = {
+    one: 1,
+    two: 2,
+    str: "23",
+    collect: {
+        name: "col",
+        number: 20,
+        add: {
+            lvl3: 15
+        }
+    }
+};
+
+function sumValue(obj){
+    let sum = 0;
+    for(key in obj)
+    {
+        if(typeof obj[key] === 'number')
+            sum += obj[key];
+        if(typeof obj[key] === 'object')
+            sum += sumValue(obj[key]);
+    }
+    return sum;
+}
+
+console.log(sumValue(fourth_inObject));
