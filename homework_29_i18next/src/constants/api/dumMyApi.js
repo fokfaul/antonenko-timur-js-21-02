7759990d3737ee5aeeaae90f4e6d55fdb66ed88e
fokfaul <fokfaul@mail.ru>
2021-12-09 21:45:48 +0200

    Локализация проекта с помощью i18next

---
+++ homework_29_i18next/src/constants/api/dumMyApi.js
@@ -0,0 +1,10 @@
+export const BASE_URL = 'https://dummyapi.io/data/v1/';
+
+export const USER_URL = "user";
+export const POST_URL = "post";
+
+export const APP_ID_VALUE = '618abb40c4818a4a49bf80de';
+
+export const APP_ID_FIELD = 'app-id';
+export const PAGE_FIELD = 'page';
+export const LIMIT_FIELD = 'limit';
---
+++ homework_29_i18next/src/locale/ru/app.js
@@ -0,0 +1,47 @@
+export const APP_RU = {
+  title: 'Анти-Навык',
+  darkTheme: 'Тёмная тема',
+  header: {
+    users: "Пользователи",
+    posts: "Посты",
+  },
+  login: {
+    exit: "Выход",
+    enter: "Вход",
+    reg: "Регистрация"
+  },
+  user:{
+    title:{
+        mr: "м-р",
+        ms: "г-жа",
+        mrs: "г-жа",
+        miss: "мисс",
+        dr: "д-р",
+    },
+    gender: "Пол",
+    birth: "День рождение",
+    reg: "Зарегистрирован",
+    phone: "Телефон",
+    email: "Эл.почта",
+    name: "Имя",
+    surname: "Фамилия",
+  },
+  userForm:{
+    title: "Регистрация",
+    login: "Войти",
+    reg: "Зарегистрироваться",
+    save: "Сохранить",
+    alert: {
+        birth: "Упс, вы ещё не родились"
+    },
+    placeholder:{
+        id: "Введите свой ID"
+    },
+    helper:{
+        reg: "Еще нет аккаунта? Зарегистрируйся!"
+    }
+  },
+  data: {
+    joiner: " в "
+  }
+};
