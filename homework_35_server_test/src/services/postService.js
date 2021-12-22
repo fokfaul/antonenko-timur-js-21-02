857ed0b068ec521936abedde005172fe792269ef
fokfaul <fokfaul@mail.ru>
2021-12-22 22:49:54 +0200

    Напесание тестов для запросов по роуту post

--- homework_35_server_test/src/services/postService.js
+++ homework_35_server_test/src/services/postService.js
@@ -4,7 +4,7 @@ const { postService: messages } = require('../constants/loggerMessages')
 const {doGetRequest, doPostRequest, doPutRequest} = require('../api/dumMyApi')
 const { POST_URL } = require('../constants/api/dumMyApi')
 const {apiError} = require('../constants/errors/dumMyApi')
-const {defaultResponse} = require('./common')
+const {defaultResponse, defaultErrorResponse} = require('./common')
 const {dateDMT, dateMDY} = require('../hooks/date')
 
 class PostService {
@@ -13,8 +13,8 @@ class PostService {
     doGetRequest(POST_URL, req.query).then((apiData) => {
         if ('error' in apiData)
         {
-            logger.info(format(messages.GET_POST_LIST_ERROR, 400, apiData))
-            res.status(400).send({"error": apiError(apiData['error'])})
+            logger.info(format(messages.GET_POST_LIST_ERROR, 520, apiData))
+            res.status(520).send({"error": apiError(apiData['error'])})
         }
         else
         {
@@ -25,15 +25,15 @@ class PostService {
             })
             res.status(200).send(apiData)
         }
-    });
+    }).catch((error) => {defaultErrorResponse(res, error, messages.GET_POST_LIST_ERROR)});
   }
   getPostById(req, res) {
     logger.info(format(messages.GET_POST_INPUT_PARAMS, req.params.id))
     doGetRequest(`${POST_URL}/${req.params.id}`).then((apiData) => {
         if ('error' in apiData)
         {
-            logger.info(format(messages.GET_POST_ERROR, 400, apiData))
-            res.status(400).send({"error": apiError(apiData['error'])})
+            logger.info(format(messages.GET_POST_ERROR, 520, apiData))
+            res.status(520).send({"error": apiError(apiData['error'])})
         }
         else
         {
@@ -41,15 +41,15 @@ class PostService {
             apiData.publishDate = dateMDY(apiData.publishDate, req.headers['accept-language']);
             res.status(200).send(apiData)
         }
-    });
+    }).catch((error) => {defaultErrorResponse(res, error, messages.GET_POST_LIST_ERROR)});;
   }
   getPostComment(req, res) {
     logger.info(format(messages.POST_COMMENT_INPUT_PARAMS, req.params.id, req.query))
     doGetRequest(`${POST_URL}/${req.params.id}/comment`, req.body).then((apiData) => {
         if ('error' in apiData)
         {
-            logger.info(format(messages.POST_COMMENT_ERROR, 400, apiData))
-            res.status(400).send({"error": apiError(apiData['error'])})
+            logger.info(format(messages.POST_COMMENT_ERROR, 520, apiData))
+            res.status(520).send({"error": apiError(apiData['error'])})
         }
         else
         {
@@ -60,7 +60,7 @@ class PostService {
             })
             res.status(200).send(apiData)
         }
-    });
+    }).catch((error) => {defaultErrorResponse(res, error, messages.GET_POST_LIST_ERROR)});;
   }
 }
 
